e26b99ec9c8167e724bb64856cca4583ebc0c0a4
USER <user@USERs-MacBook-Pro.local>
2025-01-09 13:29:29 +0700

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS UAS</title>
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
+</head>
+<body>
+    <h1 class="text-center">CRUD SISWA</h1>
+    <div class="mt-3">
+        @yield('konten')
+    </div>
+    
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TUGAS UAS</title>
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
+</head>
+<body>
+    <hi class="text-center">Selamat Datang di Aplikasi CRUD Laravel 11</hi>
+    
+</body>
+</html>
